9a5d5f706d3abb607a5a0abb6ae399338e67da7c
Alex Bush <alex.bush@coxauto.co.uk>
2018-07-24 14:14:55 +0100

    Zeppelin 0.8 changes

--- zeppelin-csd-src/scripts/control.py
+++ zeppelin-csd-src/scripts/control.py
@@ -23,6 +23,7 @@ def properties_string_to_dict(prop_string):
     ini_str = '[dummy]\n' + prop_string
     ini_fp = StringIO.StringIO(ini_str)
     config = ConfigParser.RawConfigParser()
+    config.optionxform = str
     config.readfp(ini_fp)
     return {k: v for k, v in config.items("dummy")}
 
@@ -72,10 +73,9 @@ def generate_livy_conf_struct(spark_version, interpreter_id, interpreter_name, l
     conf["id"] = interpreter_id
     conf["name"] = interpreter_name
     conf["properties"]["zeppelin.livy.url"] = livy_url
-    conf["properties"]["zeppelin.interpreter.localRepo"] = "%s/local-repo/%s" % (
-        os.environ("ZEPPELIN_DATA_DIR"), interpreter_id)
+    conf["properties"]["zeppelin.interpreter.localRepo"] = "%s/local-repo/%s" % (os.environ["ZEPPELIN_DATA_DIR"], interpreter_id)
     if "ZEPPELIN_PRINCIPAL" in os.environ.keys():
-        conf["properties"]["zeppelin.livy.principal"] = os.environ("ZEPPELIN_PRINCIPAL")
+        conf["properties"]["zeppelin.livy.principal"] = os.environ["ZEPPELIN_PRINCIPAL"]
         conf["properties"]["zeppelin.livy.keytab"] = "%SERVICENAMELOWER%.keytab"
     props = properties_string_to_dict(interpreter_properties_string)
     for k, v in props.iteritems():
@@ -100,8 +100,7 @@ def run_command(command):
         sys.exit(1)
 
 
-def start():
-    log("Attempting to start")
+def base_conf():
     log("Detected CDH_VERSION of [%s]" % os.environ["CDH_VERSION"])
 
     # Set java path
@@ -126,6 +125,17 @@ def start():
     zeppelin_conf_dir = "%s/%SERVICENAMELOWER%-conf" % base_conf_dir
     error_if_missing(zeppelin_conf_dir)
     os.environ["ZEPPELIN_CONF_DIR"] = zeppelin_conf_dir
+    os.environ["ZEPPELIN_PID_DIR"] = "%s/run" % os.environ["ZEPPELIN_LOG_DIR"]
+    os.environ["ZEPPELIN_IDENT_STRING"] = "zeppelin"
+    return (base_conf_dir, zeppelin_conf_dir)
+
+def start():
+    log("Attempting to start")
+
+    # Base config
+    (base_conf_dir, zeppelin_conf_dir) = base_conf()
+
+    #Start specific conf
     livy_conf_dir = "%s/%LIVYSERVICENAMELOWER%-conf" % base_conf_dir
     error_if_missing(livy_conf_dir)
     livy_conf_file = "%s/server.properties" % livy_conf_dir
@@ -140,7 +150,7 @@ def start():
 
     livy_conf_struct = generate_livy_conf_struct("spark", "2CYCWRZPP", "livy", server_props_string,
                                                  livy_interpreter_json_string, livy_properties_string)
-    livy2_conf_struct = generate_livy_conf_struct("spark2", "2CYCWDZPP", "livy", server_props_string,
+    livy2_conf_struct = generate_livy_conf_struct("spark2", "2CYCWDZPP", "livy2", server_props_string,
                                                   livy_interpreter_json_string, livy_properties_string)
 
     livy_interpreters = []
@@ -158,9 +168,9 @@ def start():
     with open(completed_filename, 'w+') as f:
         json.dump(interpreter_struct, f)
 
-    run_command("hdfs dfs -mkdir -p %s" % os.environ["ZEPPELIN_CONF_DIR"])
+    run_command("hdfs dfs -mkdir -p %s" % os.environ["ZEPPELIN_CONF_FS_DIR"])
 
-    run_command("hdfs dfs -put -f %s %s" % (completed_filename, os.environ["ZEPPELIN_CONF_DIR"]))
+    run_command("hdfs dfs -put -f %s %s" % (completed_filename, os.environ["ZEPPELIN_CONF_FS_DIR"]))
 
     run_command("hdfs dfs -mkdir -p %s" % os.environ["ZEPPELIN_NOTEBOOK_DIR"])
 
@@ -170,7 +180,19 @@ def start():
         shutil.move(shiro_filename, "%s.template" % shiro_filename)
 
     #Run zeppelin, this seems to leak threads, look for a way to clean up properly
-    run_command("%s/bin/zeppelin.sh" % os.environ["ZEPPELIN_HOME"])
+    run_command("%s/bin/zeppelin-daemon.sh --config %s start" % (os.environ["ZEPPELIN_HOME"], os.environ["ZEPPELIN_CONF_DIR"]))
+
+    #Wait for PID to stop
+    run_command("tail --pid=$( cat %s/zeppelin-zeppelin-*.pid) -f /dev/null" % os.environ["ZEPPELIN_PID_DIR"])
+    return
+
+def stop():
+    log("Attempting to stop")
+
+    # Base config
+    (base_conf_dir, zeppelin_conf_dir) = base_conf()
+
+    run_command("%s/bin/zeppelin-daemon.sh --config %s stop" % (os.environ["ZEPPELIN_HOME"], os.environ["ZEPPELIN_CONF_DIR"]))
     return
 
 if __name__ == '__main__':
@@ -179,7 +201,9 @@ if __name__ == '__main__':
     if len(sys.argv) < 2:
         log("No argument given")
         sys.exit(1)
-    elif sys.argv[1] != "start":
-        log("Don't understand [%s]" % sys.argv[1])
-    else:
+    elif sys.argv[1] == "start":
         start()
+    elif sys.argv[1] == "stop":
+        stop()
+    else:
+        log("Don't understand [%s]" % sys.argv[1])
